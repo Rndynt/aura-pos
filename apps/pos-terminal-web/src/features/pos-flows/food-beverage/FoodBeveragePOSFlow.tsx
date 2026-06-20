fd33d2764e978f3bef46f491bed22c8cea3b3681
Rndynt <rendyanta.maulana@gmail.com>
2026-06-21 01:24:41 +0700

    fix(pos): hide internal food beverage capability panel

--- apps/pos-terminal-web/src/features/pos-flows/food-beverage/FoodBeveragePOSFlow.tsx
+++ apps/pos-terminal-web/src/features/pos-flows/food-beverage/FoodBeveragePOSFlow.tsx
@@ -1,13 +1,7 @@
 import { RetailStandardPOSFlowView } from "@/features/pos-flows/retail";
-import { FoodBeverageOptionalPanels } from "./FoodBeverageOptionalPanels";
 import { useFoodBeveragePOSFlow } from "./useFoodBeveragePOSFlow";
 
 export function FoodBeveragePOSFlow() {
   const flow = useFoodBeveragePOSFlow();
-  return (
-    <div data-testid="food-beverage-pos-flow">
-      <FoodBeverageOptionalPanels capabilities={flow.capabilities} />
-      <RetailStandardPOSFlowView flow={flow} />
-    </div>
-  );
+  return <RetailStandardPOSFlowView flow={flow} />;
 }
